6594d652ae82b96ff7999ea068ccf4f0d684e649
jordan <jharrison1@qa.com>
2023-07-31 09:57:28 +0100

    init commit

---
+++ src/test/java/com/qa/spring/hedgehog/SpringHedgehogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.spring.hedgehog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHedgehogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
